be037b24baaabf6862b78a5cedcf6eee4845134d
WinefredoSailes <winefredosailes.stpc@gmail.com>
2026-07-25 14:31:38 +0800

    fix: use UTC time for TrackSolid timestamp

--- truck_pms/fleetops/tracksolid_import.py
+++ truck_pms/fleetops/tracksolid_import.py
@@ -47,7 +47,7 @@ class TracksolidClient:
         self._token_expiry = None
 
     def _now(self):
-        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 
     def _sign(self, params):
         sorted_keys = sorted(params.keys())
